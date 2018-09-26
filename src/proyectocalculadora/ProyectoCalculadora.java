75ffd20b62f4ee351eb0b958ff8024e8a50a8b65
BRAYAN PEREZ <42071686+brayanperez3215@users.noreply.github.com>
2018-09-26 17:37:58 -0500

    Update ProyectoCalculadora.java

--- src/proyectocalculadora/ProyectoCalculadora.java
+++ src/proyectocalculadora/ProyectoCalculadora.java
@@ -633,38 +633,11 @@ public class ProyectoCalculadora extends JFrame implements ActionListener
 };  
     
 
-  
-
-    // Variables declaration - do not modify                     
-    // End of variables declaration                   
+                    
 
 
 
 
    
 
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-   
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
-}
+    
